e0e70265be29528a864653a7a578697cc061e669
Bo Bramer <49931047+wuguishifu@users.noreply.github.com>
2022-01-16 15:14:41 -0800

    changed graph style from line to points as specified

--- Lab 6/graphs.m
+++ Lab 6/graphs.m
@@ -1,9 +1,9 @@
 clc
 clear
 
-tris_volumes =   [750, 800, 900, 950, 980, 990, 980, 980, 980];
-ONPG_volumes =   [250, 200, 100,  50,  20,  10,  20,  20,  20];
-enzyme_volumes = [ 50,  50,  50,  50,  50,  50,  30,  20,  10];
+tris_volumes =   [750, 800, 900, 950, 980, 990, 980, 980, 980]; % [uL 50 mM]
+ONPG_volumes =   [250, 200, 100,  50,  20,  10,  20,  20,  20]; % [uL 4 mg/mL]
+enzyme_volumes = [ 50,  50,  50,  50,  50,  50,  30,  20,  10]; % [uL]
 
 data_table = readtable("all_data.csv");
 for i = 1:9
@@ -17,22 +17,9 @@ trials = [5, 7, 8, 9];
 colors = ["r", "g", "b", "k"];
 for i = 1:4
     plot(data.("t" + trials(i))(:, 1), ...
-        data.("t" + trials(i))(:, 2), 'Color', colors(i))
+        data.("t" + trials(i))(:, 2), ("."+colors(i)), 'MarkerSize', 4)
 end
-title('Absorbance vs. Time for Different Enzyme Concentrations')
 xlabel('Time (s)')
 ylabel('Absorbance')
 legend({'50 \muL', '30 \muL', '20 \muL', '10 \muL'}, ...
     "Location", "southeast");
-
-% varied tris:ONPG ratio
-figure(2)
-hold on
-disp(ONPG_volumes ./ (ONPG_volumes + tris_volumes))
-trials = [1, 2, 3, 4, 5, 6];
-colors = ["r", "g", "b", "k", "m", "c"];
-for i = 1:6
-    plot(data.("t" + trials(i))(:, 1), ...
-        data.("t" + trials(i))(:, 2), 'Color', colors(i))
-end
-title('Absorbance vs. Timem for Different ')
